b610628469f0774a8cf75be205e7722d4f852703
Chaitanya Patil <chaitanyapatil3107@gmail.com>
2026-07-19 17:58:00 +0530

    Add Simon Says Game project

---
+++ Simon Says Game/page.js
@@ -0,0 +1,255 @@
+
+// let msg = "        he     llo     ";
+// console.log(msg);
+// console.log(msg.trim());
+
+
+// let password = prompt("set your password");
+// console.log(password.trim());
+
+// let str = "Chaitanya Patil";
+
+// let upper = str.toUpperCase();
+
+// console.log(upper);
+
+// console.log(str.toLowerCase());
+
+
+// console.log(str.indexOf("Patil"));
+
+
+// let msg = "               hello";
+
+// // console.log(msg.toUpperCase());
+// // console.log(msg.trim());
+
+// let newmsg = msg.toUpperCase().trim();
+// console.log(newmsg); //HELLO
+// console.log(newmsg.indexOf("H"));
+
+// console.log(newmsg.slice(0, 4));
+// console.log(newmsg.slice(-2)); //length-2 letters = LO
+
+
+// let str = "iloveCoding";
+
+// console.log(str.replace("love", "do"));
+
+// console.log(str.repeat(2));
+
+
+// let msg = "hello";
+
+// console.log(msg.toUpperCase().trim());
+
+// let name = "ApnaCOllege";
+
+// console.log(name.slice(4, 9));
+// console.log(name.indexOf("na"));
+
+// console.log(name.replace("Apna", "Our"));
+
+
+// let arr = [0, 1, 2, 3, 4, 5];
+
+// console.log(arr[4]);
+
+// console.log(arr.length);
+
+
+// let arr1 = ["chaitanya", 20, 6.3];
+
+// console.log(arr1);
+
+// console.log(arr1.length);
+
+// console.log(arr1[2]);
+
+// let emp = [];
+
+// console.log(emp);
+
+// console.log(arr[3][4]);
+
+// console.log(arr1[0].length);
+
+
+// let name = "chaitanya";
+
+
+// console.log(name);
+
+// let fruits = ["mango", "banana", "litchi"];
+
+// console.log(fruits);
+
+// fruits[0] = "apple";
+
+// console.log(fruits);
+
+
+// fruits[10] = "papaya";
+
+// console.log(fruits);
+
+// console.log(fruits[8]);
+
+
+// let cars = ["audi", "mercedes", "toyota", "porsche"];
+
+// console.log(cars.push("suzuki")); //add at last
+
+// console.log(cars);
+
+// console.log(cars.push("landrover"));
+
+// console.log(cars);
+
+// console.log(cars.pop()); //removes last
+
+// console.log(cars.shift()); //removes first
+
+// console.log(cars);
+
+// console.log(cars.unshift("tesla")); //add at start
+
+
+// console.log(cars);
+
+// let followers = ["a", "b", "c"];
+
+// console.log(followers);
+
+// console.log(followers.shift());
+
+// console.log(followers);
+
+
+// let months = ["january", "july", "march", "august"];
+
+// console.log(months);
+
+// console.log(months.shift());
+
+// console.log(months);
+
+
+// console.log(months.shift());
+
+// console.log(months);
+
+
+
+// console.log(months.unshift("june"));
+
+// console.log(months);
+
+
+// console.log(months.unshift("july"));
+
+// console.log(months);
+
+// let char = ["a", "b", "c", "d", "e"];
+
+// console.log(char);
+
+// console.log(char[0]);
+
+// console.log(char.indexOf("z"));
+
+// console.log(char.includes("a"));
+
+
+// let primary = ["a", "b", "c", "d", "e"];
+
+// let secondary = ["f", "g", "h", "i", "j"];
+
+
+
+// console.log(primary.concat(secondary));
+
+// let primary = ["a", "b", "c", "d", "e"];
+
+// console.log(primary.splice(-1, 1));
+
+// console.log(primary);
+
+
+// let arr = ["mon", "tues", "wed", "thurs", "friday", "z", "a"];
+
+// console.log(arr.sort());
+
+// let marks = [100, 99, 90, 23, 42, 21, 44];
+
+// console.log(marks.sort());
+
+// let start = ['january', 'july', 'march', 'august'];
+
+// start.splice(0, 2, 'july', 'june');
+
+// console.log(start);  //['july', 'june', 'march', 'august']
+
+// let lang = ["C", "C++", "HTML", "JavaScript", "Python", "Java", "C#", 'sql'];
+
+// console.log(lang.reverse().indexOf("JavaScript"));
+
+// let arr = ['a', 'b', 'c', 'e'];
+
+// let arrCopy = arr;
+
+// console.log(arrCopy);
+
+// console.log(arrCopy.push('d'));
+
+// console.log(arr);
+
+// console.log(arrCopy);
+
+
+// const arr = [1, 3, 4];
+
+// console.log(arr);
+
+// console.log(arr.push(5));
+
+// console.log(arr);
+
+// console.log(arr.shift());
+
+// console.log(arr);
+
+
+
+
+
+
+// let nums = [[2, 4], [3, 6, 2], [4, 8]];
+
+// console.log(nums);
+
+
+
+// let num = prompt("Enter a  number");
+
+// for (let i = 1; i <= 10; i++) {
+
+
+//     console.log(i * num);
+// }
+
+// let fruits = ["mango", "apple", "banana", "orange", "lithchi"];
+
+// for (i of fruits) {
+//     console.log(i);
+// }
+
+// for (c of "chaitanya") {
+//     console.log(c);
+// }
+
+// let arr = [10, 20, 30];
+
+// for (let value of arr) {
+//     console.log(value);
+// }
